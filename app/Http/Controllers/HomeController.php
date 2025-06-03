8ec7715e27a02ce6710a33a80e62eb3de14ffa1e
Chelsea Mukhanyi <chelseam@localhost.localdomain>
2025-06-03 15:08:29 +0300

    Initial commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+   public function index()
+   {
+    return view('home');
+   }
+   public function about()
+   {
+      return view('about');
+   }
+    public function skills()
+   {
+      return view('skills');
+   }
+   public function education()
+   {
+      return view('education');
+   }
+    public function experience()
+   {
+      return view('experience');
+   }
+
+     public function contact()
+   {
+      return view('contact');
+   }
+    public function blog()
+   {
+      return view('blog');
+   }
+   public function portfoli()
+   {
+    return view('portfoli');
+   }
+}
+
